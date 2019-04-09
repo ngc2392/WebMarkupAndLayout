717cc8179ba4a16f6cc2426d3cd54a802346cf51
ngc2392 <phillicon4733@gmail.com>
2019-04-08 21:01:36 -0400

    changes to home page

--- RestaurantReDesign/js/script.js
+++ RestaurantReDesign/js/script.js
@@ -1,6 +1,67 @@
 if ($('body').hasClass('home-page-body')) {
 
     var slideIndex = 1;
+    showSlides(1);
+
+    // can also use .querySelector
+    let sliderArrowPrev = document.getElementsByClassName("prev-arrow")[0];
+    let sliderArrowNext = document.getElementsByClassName("next-arrow")[0];
+    
+
+    sliderArrowPrev.addEventListener("click", function(e) {
+
+        showSlides(slideIndex += -1);
+
+
+       // console.log(this);
+        
+
+    });
+
+    sliderArrowNext.addEventListener("click", function(e) {
+
+        showSlides(slideIndex += 1);
+
+
+       // console.log(this);
+    
+    });
+    
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    // n is the slide index of the slide that is about to show  
+    function showSlides(n) {
+
+        // var is scoped to the entire function.  Let is scoped to the enclosing block.
+
+        //var i;
+        const slides = document.getElementsByClassName("slide");
+        const dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { 
+            slideIndex = slides.length;
+        }
+        if (n < 1) { 
+            //slideIndex = slides.length; 
+            slideIndex = 1;
+            return;
+        }
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+
+    }
+
+
+    /*
+    var slideIndex = 1;
     showSlides(slideIndex);
 
     function plusSlides(n) {
@@ -15,8 +76,12 @@ if ($('body').hasClass('home-page-body')) {
         var i;
         var slides = document.getElementsByClassName("slide");
         var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
+        if (n > slides.length) { 
+           // slideIndex = 1; 
+        }
+        if (n < 1) { 
+            slideIndex = slides.length; 
+        }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
@@ -26,4 +91,5 @@ if ($('body').hasClass('home-page-body')) {
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
     }
+    */
 }
