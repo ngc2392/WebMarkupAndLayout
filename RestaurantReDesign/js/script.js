9006ac270eba010ab834de8c6459bfc5fdb1ed80
ngc2392 <phillicon4733@gmail.com>
2019-05-06 04:24:39 -0400

    changes

--- RestaurantReDesign/js/script.js
+++ RestaurantReDesign/js/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+
+    // Smooth Scroll.  From https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+
+
 // mobile navigation
 document.getElementsByClassName("mobile-nav-icon")[0].addEventListener('click', function(e) {
       
@@ -124,3 +144,140 @@ if ($('body').hasClass('home-page-body')) {
     }
     */
 }
+
+if($('body').hasClass('drinks-page-body')){
+
+    $(function() {
+        // generate beer grid
+        generateDrinkList(drinks);
+    });
+
+
+    var drinks = [
+        {
+            "id": 0, 
+            "name": "Guinness Draught",
+            "type": "Stout - Irish Dry", 
+            "abv": "4.2", 
+            "ibu": "45", 
+            "company": "Guinness",
+            "location": "St. James's Gate, Dublin", 
+            "description": "Swirling clouds tumble as the storm begins to calm. Settle. Breathe in the moment, then break through the smooth, light head to the bittersweet reward."
+        }, {
+            "id": 1, 
+            "name": "Shiner Ruby Redbird ",
+            "type": "Fruit Beer", 
+            "abv": "4.01", 
+            "ibu": "13", 
+            "company": "Spoetzl Brewery  ",
+            "location": "Shiner, TX", 
+            "description": "Brewed with genuine Texas Rio Red Grapefruit, the signature sweet citrus of the Rio Grande Valley, and ginger. This lager beer features grapefruit tartness and finishes with a 'lil kick of ginger. With Texas Ruby Red Grapefruit and Ginger, a crisp and refreshing summer beer."
+        }, {
+            "id": 2, 
+            "name": "Lite ",
+            "type": "Lager - American Light", 
+            "abv": "4.2", 
+            "ibu": "10", 
+            "company": "Miller Brewing Company  ",
+            "location": "Milwaukee, WI  ", 
+            "description": "Our flagship brand, Miller Lite, is the great tasting, less filling beer that defined the American light beer category in 1975. We deliver a clear, simple message to consumers:"
+        }, {
+            "id": 3, 
+            "name": "The Hemperor HPA ",
+            "type": "IPA - American", 
+            "abv": "7", 
+            "ibu": "55", 
+            "company": "New Belgium Brewing Company",
+            "location": "Fort Collins, CO", 
+            "description": "Get ready: The Hemperor HPA, an exciting new style, will change the way you think about hoppy beers. We found a unique way to recreate the flavors of hemp with a beer that complements the inclusion of hop flavors and hemp hearts (seeds) to deliver something new, delicious and extremely dank."
+        }, {
+            "id": 4, 
+            "name": "Misadventures And Missteps ",
+            "type": "Belgian Strong Golden Ale", 
+            "abv": "10", 
+            "ibu": "", 
+            "company": "Neshaminy Creek Brewing Company", 
+            "location": "Croydon, PA", 
+            "description": "Belgian Golden Strong brewed with Lemongrass and Pink Peppercorns"
+        }, {
+            "id": 5, 
+            "name": "Summer Crush",
+            "type": "Pale Wheat Ale - American", 
+            "abv": "5", 
+            "ibu": "", 
+            "company": "Yards Brewing Co.",
+            "location": "Philadelphia, PA", 
+            "description": "Summer Crush is an easy drinking, flavorful Wheat Beer with a juicy citrus finish that transports you to bright summer days and hot summer nights. Brewed with orange and lime zest, this crushable delight brings the refreshment to the shore, the front stoop, the rooftop, and everywhere else you celebrate summer."
+        }
+    ];
+
+    }
+
+    var generateDrinkList = function(array) {
+        array.forEach((element, index) => {
+            const drinkCard = document.createElement('div');
+
+            const markup = `
+                <div class="beer-card">
+                    <div class="header">
+                        <div class="name">${element.name}</div>
+                        <div class="type">${element.type}</div>
+                    </div>
+                    
+                    <div class="info">
+                        <div class="abv">${element.abv}% ABV</div>
+                        <div class="ibu">${element.ibu} IBU</div>
+                        <div class="company">${element.company}</div>
+                        <div class="location">${element.location}</div>
+                    </div>
+
+                    <div class="description">${element.description}</div>
+                
+                </div>
+            `;
+
+            drinkCard.innerHTML = markup;
+            drinksGrid = document.querySelector('.drinks-page-body .beer-grid')
+            drinksGrid.appendChild(drinkCard);
+        });
+    }
+
+    if($('body').hasClass('menu-page-body')) {
+        document.querySelector('.scroll-to').addEventListener('click', function() {
+            
+            arrow = this.children[0];
+            // difference between .childNodes and .children: https://www.w3schools.com/jsref/prop_element_children.asp
+            linkBox = this.children[2];
+
+
+            if (linkBox.offsetHeight > 20) {
+                linkBox.classList.remove('link-box-grow');
+                arrow.classList.remove('fa-chevron-down');
+                arrow.classList.add('fa-chevron-up');
+            } else {
+                
+                linkBox.classList.add('link-box-grow');
+                arrow.classList.remove('fa-chevron-up');
+                arrow.classList.add('fa-chevron-down');
+
+            }
+
+           
+        });
+
+        document.querySelectorAll('.link-box > span').forEach((element, index) => {
+            element.addEventListener('click', function() {
+
+                const sectionToGoTo = this.dataset.scrollto;
+
+                document.querySelector('.' + sectionToGoTo).scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+            });
+        });
+
+    }
+
+});
+
